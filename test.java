609ca0f259d00d96c0f084a01ce19f33370b7e78
tachi7830 <xwsmilexw@gmail.com>
2022-01-24 14:47:30 +0900

    progate:Java lesson1

--- test.java
+++ test.java
@@ -1,18 +1,19 @@
 class Main {
   public static void main(String[] args) {
-    int number = 8;
+    int length = 6;
+    int height = 8;
     
-    // 変数numberに7をかけて、変数numberを上書きしてください
-    number *= 7;
+    // 変数rectangleAreaに、四角形の面積を代入してください
+    int rectangleArea = length * height;
     
-    // 変数numberを出力してください
-    System.out.println(number);
+    // 変数rectangleAreaを出力してください
+    System.out.println(rectangleArea);
     
-    // 変数numberの値に1を足して、変数numberを上書きしてください
-    number ++;
+    // 変数triangleAreaに、三角形の面積を代入してください
+    int triangleArea = length * height / 2;
     
-    // 変数numberを出力してください
-    System.out.println(number);
+    // 変数triangleAreaを出力してください
+    System.out.println(triangleArea);
     
   }
 }
