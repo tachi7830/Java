783a21fab5acce84cac64451ff691cd515ed446d
tachi7830 <xwsmilexw@gmail.com>
2022-01-25 16:36:43 +0900

    progate:Java lesson2

--- test.java
+++ test.java
@@ -1,22 +1,19 @@
 class Main {
   public static void main(String[] args) {
-    // 「true」を用いて、「真」を表す真偽値を出力してください
-    System.out.println(true);
+    // trueと出力されるようにしてください
+    System.out.println(true || false);
     
-    // 「false」を用いて、「偽」を表す真偽値を出力してください
-    System.out.println(false);
+    // falseと出力されるようにしてください
+    System.out.println(false && true);
     
-    // 「==」を用いて、値を比較した結果を出力してください
-    System.out.println(12 / 4 == 3);
+    // 「8 < 5」かつ「3 >= 2」の結果を出力してください
+    System.out.println(8 < 5 && 3 >= 2);
     
-    // 「!=」を用いて、値を比較した結果を出力してください
-    System.out.println(12 / 4 != 3);
+    // 「8 < 5」または「3 >= 2」の結果を出力してください
+    System.out.println(8 < 5 || 3 >= 2);
     
-    // 変数を定義し、値を比較した結果を代入してください
-    boolean bool = 3 * 9 == 27;
-    
-    // 変数boolの値を出力してください
-    System.out.println(bool);
+    // 「8 < 5」でない、の結果を出力してください
+    System.out.println(!(8 < 5));
     
   }
 }
