b76ae924f2219faac7fb279fe57707ff007ff4d7
tachi7830 <xwsmilexw@gmail.com>
2022-01-27 16:53:28 +0900

    progate:Java lesson2

--- test.java
+++ test.java
@@ -1,14 +1,17 @@
 class Main {
   public static void main(String[] args) {
-    int n = 1;
+    int n = 0;
     
-    // switch文を用いて、変数nの値に応じて条件分岐をしてください
-    switch(n){
+    switch (n) {
       case 1:
         System.out.println("大吉です");
-      break;
+        break;
       case 2:
-        System.out.println("吉です");
+        System.out.println("中吉です");
+        break;
+      // defaultを用いて、変数nの値がどのcaseにも合致しない場合の処理を記述してください
+      default:
+        System.out.println("凶です");
       break;
     }
   }
