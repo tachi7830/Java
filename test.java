66fafb20cd211d356378a4762663ce597f80d509
tachi7830 <xwsmilexw@gmail.com>
2022-01-27 21:25:50 +0900

    progate:Java lesson2

--- test.java
+++ test.java
@@ -1,8 +1,25 @@
 class Main {
   public static void main(String[] args) {
-    // for文を用いて、繰り返し処理をつくってください
-    for(int i = 1; i <= 10; i++ ){
-      System.out.println(i + "回目のループです")
+    System.out.println("=== while文 ===");
+    int i = 1;
+    while (i < 10) {
+      // iが5の倍数のとき、繰り返し処理を終了してください
+      if(i % 5 == 0){
+        break;
+      }
+      
+      System.out.println(i);
+      i++;
+    }
+    
+    System.out.println("=== for文 ===");
+    for (int j = 1; j < 10; j++) {
+      // jが3の倍数のとき、処理をスキップしてください
+      if(j % 3 == 0){
+        continue;
+      }
+      
+      System.out.println(j);
     }
   }
 }
