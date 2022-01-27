471d97747fc03a8082c04b0db9acde524a82e6be
tachi7830 <xwsmilexw@gmail.com>
2022-01-27 21:29:46 +0900

    progate:Java lesson2

--- test.java
+++ test.java
@@ -1,25 +1,13 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("=== while文 ===");
-    int i = 1;
-    while (i < 10) {
-      // iが5の倍数のとき、繰り返し処理を終了してください
-      if(i % 5 == 0){
-        break;
-      }
-      
-      System.out.println(i);
-      i++;
-    }
+    // 変数namesに、配列を代入してください
+    String[] names = {"にんじゃわんこ","ひつじ仙人", "ベイビーわんこ"};
     
-    System.out.println("=== for文 ===");
-    for (int j = 1; j < 10; j++) {
-      // jが3の倍数のとき、処理をスキップしてください
-      if(j % 3 == 0){
-        continue;
-      }
-      
-      System.out.println(j);
-    }
+    // インデックス番号が0の要素を出力してください
+    System.out.println(names[0]);
+    
+    // インデックス番号が2の要素を出力してください
+    System.out.println(names[2]);
+    
   }
 }
