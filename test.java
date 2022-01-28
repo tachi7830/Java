e716f10111643df193c9f33c62f5bdb6358db81e
tachi7830 <xwsmilexw@gmail.com>
2022-01-28 13:03:02 +0900

    progate:Java lesson3

--- test.java
+++ test.java
@@ -1,13 +1,15 @@
 class Main {
   public static void main(String[] args) {
-    // printDataメソッドを呼び出してください
-    printData();
+    // それぞれ年齢に関する引数を追加してください
+    printData("Kate Jones", 27 );
+    printData("John Christopher Smith", 65 );
+  }
+
+  // 年齢に関する引数を受け取れるようにしてください
+  public static void printData(String name, int age) {
+    System.out.println("私の名前は" + name + "です");
+    // 「年齢は◯◯歳です」と出力してください
+    System.out.println("年齢は" + age +"歳です");
     
   }
-  
-  // printDataメソッドを定義してください
-    public static void printData(){
-      System.out.println("私の名前はKate Jonesです");
-    }
-  
 }
