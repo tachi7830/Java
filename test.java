fa11dfa850e7813952dec26900ccfa3a548cf818
tachi7830 <xwsmilexw@gmail.com>
2022-01-28 12:55:53 +0900

    progate:Java lesson3

--- test.java
+++ test.java
@@ -1,10 +1,13 @@
 class Main {
   public static void main(String[] args) {
-    hello();
+    // printDataメソッドを呼び出してください
+    printData();
+    
   }
   
-  public static void hello() {
-    // "Hello World"を、"Hello Java"に書き換えてください
-    System.out.println("Hello Java");
-  }
+  // printDataメソッドを定義してください
+    public static void printData(){
+      System.out.println("私の名前はKate Jonesです");
+    }
+  
 }
