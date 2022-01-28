f8f8a15e337b6d55c5f019429e99b02379fb67dc
tachi7830 <xwsmilexw@gmail.com>
2022-01-28 12:50:52 +0900

    progate:Java lesson3 start

--- test.java
+++ test.java
@@ -1,21 +1,10 @@
 class Main {
   public static void main(String[] args) {
-    // 変数numbersに、与えられた数字の配列を代入してください
-    int numbers[] = {1, 4, 6, 9, 13, 16};
-    
-    int oddSum = 0;
-    int evenSum = 0;
-    
-    // for文を用いて、配列numbersの偶数の和と奇数の和を求めてください
-    for(int i = 0; i < numbers.length; i++){
-      if(numbers[i] % 2 == 0){
-        evenSum += numbers[i];
-      } else {
-        oddSum += numbers[i];
-      }
-    }
-
-    System.out.println("奇数の和は" + oddSum + "です");
-    System.out.println("偶数の和は" + evenSum + "です");
+    hello();
+  }
+  
+  public static void hello() {
+    // "Hello World"を、"Hello Java"に書き換えてください
+    System.out.println("Hello Java");
   }
 }
