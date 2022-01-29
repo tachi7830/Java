6a1a44495d9f00bd5582f268b3940b0cc28b2572
tachi7830 <xwsmilexw@gmail.com>
2022-01-29 23:39:46 +0900

    progate:Java lesson3 finish

--- test.java
+++ test.java
@@ -1,15 +1,29 @@
-// java.util.Scannerを読み込んでください
 import java.util.Scanner;
+
 class Main {
-  public static void main (String[] args) {
+  public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     
-    System.out.print("名前： ");
+    System.out.print("名前：");
+    // 文字列の入力を受け取ってください
+    String firstName = scanner.next();
     
-    // 変数nameを定義し、コンソールから文字列を受け取って代入してください
-    String name = scanner.next();
+    System.out.print("名字：");
+    // 文字列の入力を受け取ってください
+    String lastName = scanner.next();
     
-    // 「こんにちは◯◯さん」と出力してください
-    System.out.println("こんにちは" + name + "さん");
+    System.out.print("年齢：");
+    // 整数の入力を受け取ってください
+    int age = scanner.nextInt();
+    
+    System.out.print("身長(m)：");
+    // 小数の入力を受け取ってください
+    double height = scanner.nextDouble();
+    
+    System.out.print("体重(kg)：");
+    // 小数の入力を受け取ってください
+    double weight = scanner.nextDouble();
+    
+    Person.printData(Person.fullName(firstName, lastName), age, height, weight);
   }
 }
