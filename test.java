c119051d94a0bb2944fbb4c337b7ea8f791a7cb3
tachi7830 <xwsmilexw@gmail.com>
2022-01-29 23:11:09 +0900

    progate:Java lesson3

--- test.java
+++ test.java
@@ -1,23 +1,33 @@
 class Main {
   public static void main(String[] args) {
-    printData(fullName("Kate", "Jones"), 27);
-    
-    // fullNameメソッドを用いて、printDataの引数を書き換えてください
-    printData(fullName("John", "Christopher", "Smith"), 65);
+    printData(fullName("Kate", "Jones"), 27, 1.6, 50.0);
+    printData(fullName("John", "Christopher", "Smith"), 65, 1.75, 80.0);
   }
 
-  public static void printData(String name, int age) {
+  public static void printData(String name, int age, double height, double weight) {
     System.out.println("私の名前は" + name + "です");
     System.out.println("年齢は" + age + "歳です");
+    System.out.println("身長は" + height + "mです");
+    System.out.println("体重は" + weight + "kgです");
+    
+    // 変数bmiに、bmiメソッドを用いてBMIを代入してください
+    double bmi = bmi(height,weight);
+    
+    // 変数bmiを用いて、「BMIは◯◯です」と出力してください
+    System.out.println("BMIは" + bmi + "です");
+    
   }
 
   public static String fullName(String firstName, String lastName) {
     return firstName + " " + lastName;
   }
   
-  // fullNameメソッドを定義してください
-  public static String fullName(String firstName, String middleName, String lastName){
-    return firstName + "" + middleName + "" + lastName;
+  public static String fullName(String firstName, String middleName, String lastName) {
+    return firstName + " " + middleName + " " + lastName;
   }
   
+  // bmiメソッドを定義してください
+  public static double bmi(double height, double weight){
+    return weight / height / height;
+  }
 }
