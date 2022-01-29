a668f1331c2a83a4ceb0477d4c6e54f53d234d50
tachi7830 <xwsmilexw@gmail.com>
2022-01-29 23:19:39 +0900

    progate:Java lesson3

--- test.java
+++ test.java
@@ -9,25 +9,33 @@ class Main {
     System.out.println("年齢は" + age + "歳です");
     System.out.println("身長は" + height + "mです");
     System.out.println("体重は" + weight + "kgです");
-    
-    // 変数bmiに、bmiメソッドを用いてBMIを代入してください
-    double bmi = bmi(height,weight);
-    
-    // 変数bmiを用いて、「BMIは◯◯です」と出力してください
+    double bmi = bmi(height, weight);
     System.out.println("BMIは" + bmi + "です");
+    
+    // isHealthyメソッドの結果で条件分岐を行ってください
+    if (isHealthy(bmi)){
+      System.out.println("健康です");
+    } else {
+      System.out.println("健康ではありません");
+    }
     
   }
 
   public static String fullName(String firstName, String lastName) {
     return firstName + " " + lastName;
   }
-  
+
   public static String fullName(String firstName, String middleName, String lastName) {
     return firstName + " " + middleName + " " + lastName;
   }
-  
-  // bmiメソッドを定義してください
-  public static double bmi(double height, double weight){
+
+  public static double bmi(double height, double weight) {
     return weight / height / height;
   }
+
+  // isHealthyメソッドを定義してください
+  public static boolean isHealthy(double bmi){
+    return bmi >= 18.5  && bmi < 25.0;
+  }
+  
 }
