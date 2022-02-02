bb8f81f5899d6a635442d8b68fe58dcb6d7758f6
tachi7830 <xwsmilexw@gmail.com>
2022-02-02 20:37:56 +0900

    progate:Java lesson4

--- Person.java
+++ Person.java
@@ -1,8 +1,15 @@
 class Person {
-  // インスタンスフィールドnameを定義してください
   public String name;
 
+  // コンストラクタがString型の引数を受け取るようにしてください
+  Person(String name) {
+    System.out.println("インスタンスが生成されました");
+    // インスタンスフィールドnameに値をセットしてください
+    this.name = name;
+    
+  }
+
   public void hello() {
-    System.out.println("こんにちは");
+    System.out.println("こんにちは、私は" + this.name + "です");
   }
 }
