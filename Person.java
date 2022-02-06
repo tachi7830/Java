74d8317b64cdbf009a1ea9f7684550be03411c62
tachi7830 <xwsmilexw@gmail.com>
2022-02-06 20:09:09 +0900

    progate:Java lesson4

--- Person.java
+++ Person.java
@@ -1,11 +1,16 @@
 class Person {
+  // int型のクラスフィールドcountを定義し、0を代入してください
+  public static int count = 0;
+  
   public String firstName;
   public String lastName;
   public int age;
-  public double height;
-  public double weight;
+  public double height, weight;
 
   Person(String firstName, String lastName, int age, double height, double weight) {
+    // 変数countに1を足してください
+    Person.count ++;
+    
     this.firstName = firstName;
     this.lastName = lastName;
     this.age = age;
@@ -21,11 +26,9 @@ class Person {
     return this.weight / this.height / this.height;
   }
   
-  // printDataメソッドを定義してください
   public void printData() {
     System.out.println("私の名前は" + this.fullName() + "です");
     System.out.println("年齢は" + this.age + "歳です");
     System.out.println("BMIは" + Math.round(this.bmi()) + "です");
   }
-  
 }
