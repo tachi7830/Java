43d436762ffa9ebc25a8bb3e71be902cc4c2999f
tachi7830 <xwsmilexw@gmail.com>
2022-02-08 23:58:18 +0900

    progate:Java lesson2

--- Main.java
+++ Main.java
@@ -1,11 +1,21 @@
 class Main {
   public static void main(String[] args) {
-    String[] names = {"にんじゃわんこ", "ひつじ仙人", "ベイビーわんこ"};
+    // 変数numbersに、与えられた数字の配列を代入してください
+    int[] numbers = {1, 4, 6, 9, 13, 16};
     
-    // 配列用のfor文を用いて、「私の名前は◯◯です」と出力してください
-    for (String name: names){
-      System.out.println("私の名前は" + name + "です");
+    int oddSum = 0;
+    int evenSum = 0;
+    
+    // for文を用いて、配列numbersの偶数の和と奇数の和を求めてください
+    for (int number: numbers){
+      if (number % 2 == 0){
+        evenSum += number;
+      } else {
+        oddSum += number;
+      }
     }
-    
+
+    System.out.println("奇数の和は" + oddSum + "です");
+    System.out.println("偶数の和は" + evenSum + "です");
   }
 }
