aee8b7ad270329379280aa4a3c22e464519c6e98
tachi7830 <xwsmilexw@gmail.com>
2022-02-11 23:06:25 +0900

    dmm lesson5 finish

--- Java.DMM/src/Ready02.java
+++ Java.DMM/src/Ready02.java
@@ -1,24 +1,18 @@
 public class Ready02 {
   public static void main(String[] args) {
-  long number1 = 1333333L;
-  System.out.println(number1);
-  
-  double number2 = 2.23;
-  System.out.println(number2);
-  
-  boolean test = true;
-  System.out.println(test);
-  
-  Float number3 = 3.636340f;
-  System.out.println(number3);
-  
-  Integer number4 = 5;
-  System.out.println(number4);
-  
-  Character str = 'あ';
-  System.out.println(str);
-  
-  int[] number5 = new int[] {10, 5, 4 ,6 };
-  System.out.println(number5[3]);
+    int val = 2 + 5;
+    System.out.println(val);
+    
+    val++;
+    System.out.println(val);
+    
+    val *= 5;
+    System.out.println(val);
+    
+    boolean bool = (val == 50);
+    System.out.println(bool);
+    
+    bool = (val < 50) && (val % 10 ==0);
+    System.out.println(bool);
   }
 }
