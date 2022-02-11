b011c961e0e3ad6cbec7f5a585b6a6102fd53661
tachi7830 <xwsmilexw@gmail.com>
2022-02-11 22:35:27 +0900

    dmm lesson4 finish

--- Java.DMM/src/Ready02.java
+++ Java.DMM/src/Ready02.java
@@ -1,15 +1,24 @@
 public class Ready02 {
- public static void main(String[] args) {
-    String string1;
-    
-    string1 = "あいう";
-    System.out.println(string1 + "の文字数" + string1.length());
-    
-    string1 = "";
-    System.out.println(string1 + "の文字数" + string1.length());
-    
-    string1 = null;
-    System.out.println(string1 + "の文字数" + string1.length());
-    
- }
+  public static void main(String[] args) {
+  long number1 = 1333333L;
+  System.out.println(number1);
+  
+  double number2 = 2.23;
+  System.out.println(number2);
+  
+  boolean test = true;
+  System.out.println(test);
+  
+  Float number3 = 3.636340f;
+  System.out.println(number3);
+  
+  Integer number4 = 5;
+  System.out.println(number4);
+  
+  Character str = 'あ';
+  System.out.println(str);
+  
+  int[] number5 = new int[] {10, 5, 4 ,6 };
+  System.out.println(number5[3]);
+  }
 }
