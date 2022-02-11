d93854fc50e0d2d1e6711ba48f7fedc5608d3893
tachi7830 <xwsmilexw@gmail.com>
2022-02-11 22:00:52 +0900

    dmm lesson4

--- Java.DMM/src/Ready02.java
+++ Java.DMM/src/Ready02.java
@@ -1,16 +1,12 @@
 public class Ready02 {
   public static void main(String[] args) {
-	int num1;
-	String str1;
-	num1 = 10;
-	str1 = "Hello World";
-	System.out.println("num1");
-	System.out.println("str1");
-	
-	num1 = 20;
-	System.out.println(num1);
-	
-	int num2 = 30;
-	System.out.println(num2);
+	  String name = "yamada";
+	  System.out.println(name);
+	  
+	  name = "tanaka";
+	  System.out.println(name);
+	  
+	  final int number = 7;
+	  System.out.println(number);
   }
 }
