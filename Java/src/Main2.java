432a959c54b3fd1cd9a0d5b660c36bdddbe9e25c
tachi7830 <xwsmilexw@gmail.com>
2022-02-12 23:50:21 +0900

    java book lesson2

--- Java/src/Main2.java
+++ Java/src/Main2.java
@@ -1,11 +1,9 @@
 
 public class Main2 {
   public static void main(String[] args) {
-	for(int i = 1; i <= 10; i++) {
-		if(i>=5) {
-			break;
-		}
-		System.out.println(i);
-	}
+	  String name = "山田";
+	  System.out.print("私の名前は");
+	  System.out.print(name);
+	  System.out.print("です");
   }
 }
