395ffa309b7d8b0e0894b416ca2816c47609800c
tachi7830 <xwsmilexw@gmail.com>
2022-02-13 00:02:43 +0900

    java book lesson3

--- Java/src/Main2.java
+++ Java/src/Main2.java
@@ -1,9 +1,9 @@
 
 public class Main2 {
   public static void main(String[] args) {
-	  String name = "山田";
-	  System.out.print("私の名前は");
-	  System.out.print(name);
-	  System.out.print("です");
+	 boolean doorClose = true;
+	 while (doorClose == true) {
+		 System.out.println("ドアをノックする");
+	 }
   }
 }
