f2c0cc6ca62e252601c9cbc178da191762661255
tachi7830 <xwsmilexw@gmail.com>
2022-02-12 11:40:01 +0900

    dmm lesson9

--- Java.DMM/src/Ready02.java
+++ Java.DMM/src/Ready02.java
@@ -1,21 +1,9 @@
 public class Ready02 {
    public static void main(String[] args) {
-	   int number = 1;
-	   while (number < 5) {
-		   System.out.println(number * number);
-		   number++;
-	   }
+	   Human01 yamada = new Human01();
+	   System.out.println("名前は" + yamada.name + "で、年齢は" + yamada.age + "です");
 	   
-	   int[] numbers = {7,20,15,40};
-	   for (int num = 0; num < numbers.length; num++) {
-		   System.out.println(numbers[num]);
-	   }
-	   
-	   for (int val : numbers) {
-		   if (val % 2 == 0) {
-			   continue;
-		   }
-		   System.out.println(val);
-	   }
+	   Human01 sato = new Human01("佐藤",26);
+	   System.out.println("名前は" + sato.name + "で、年齢は" + sato.age + "です");
    }
 }
