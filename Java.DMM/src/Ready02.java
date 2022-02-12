e43b55173d0e0b6d9bb5572d6def0f97439d8118
tachi7830 <xwsmilexw@gmail.com>
2022-02-12 10:07:10 +0900

    dmm lesson7

--- Java.DMM/src/Ready02.java
+++ Java.DMM/src/Ready02.java
@@ -1,18 +1,14 @@
 public class Ready02 {
   public static void main(String[] args) {
-    int val = 2 + 5;
-    System.out.println(val);
-    
-    val++;
-    System.out.println(val);
-    
-    val *= 5;
-    System.out.println(val);
-    
-    boolean bool = (val == 50);
-    System.out.println(bool);
-    
-    bool = (val < 50) && (val % 10 ==0);
-    System.out.println(bool);
+	  int number = 7;
+	  if (number < 5) {
+		  System.out.println(number + "は5未満です");
+	  }
+	  if (number < 10) {
+		  System.out.println(number + "は10未満です");
+	  }
+	  if (number < 15) {
+		  System.out.println(number + "は15未満です");
+	  }
   }
 }
