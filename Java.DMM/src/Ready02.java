699a239c5cf3403bbbb83f20c8bcd7f8dd06de99
tachi7830 <xwsmilexw@gmail.com>
2022-02-12 10:49:29 +0900

    dmm lesson8

--- Java.DMM/src/Ready02.java
+++ Java.DMM/src/Ready02.java
@@ -1,9 +1,9 @@
 public class Ready02 {
   public static void main(String[] args) {
 	  int number = 1;
-	  while (number < 50) {
+	  do {
 		  number *= 2;
 		  System.out.println("while01 = " + number);
-	  }
+	  } while (number < 50);
   }
 }
