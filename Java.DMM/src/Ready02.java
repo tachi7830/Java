1fbf93612766729fe99b68dc920d6b05d7ec4131
tachi7830 <xwsmilexw@gmail.com>
2022-02-12 10:40:41 +0900

    dmm lesson7 finish

--- Java.DMM/src/Ready02.java
+++ Java.DMM/src/Ready02.java
@@ -1,14 +1,19 @@
 public class Ready02 {
   public static void main(String[] args) {
-	  int distance = 20;
-	  if (distance <= 5) {
-		  System.out.println("とても近いです");
-	  } else if (distance <= 10) {
-		  System.out.println("近いです");
-	  } else if (distance <= 15) {
-		  System.out.println("頑張ろう");
-	  } else {
-		  System.out.println("とても遠いです");
+	  String color = "green";
+	  
+	  switch (color) {
+	    case "red":
+	    	System.out.println("赤信号です");
+	    break;
+	    case "yellow":
+	    	System.out.println("黄色信号です");
+	    break;
+	    case "blue":
+	    	System.out.println("青信号です");
+	    break;
+	    default :
+	    	System.out.println("信号の色ではありません");
 	  }
   }
 }
