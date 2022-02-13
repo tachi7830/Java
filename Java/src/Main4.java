17a80c783f165cacfc6df391d2abcc34d9dcd5c7
tachi7830 <xwsmilexw@gmail.com>
2022-02-13 14:53:02 +0900

    java book lesson3

--- Java/src/Main4.java
+++ Java/src/Main4.java
@@ -1,11 +1,14 @@
 public class Main4 {
-  public static void main(String[] args) {
-	  for (int i = 1; i <10; i++) {
-		  for (int j =1; j < 10; j++) {
-			  System.out.print(i * j);
-			  System.out.print("");
-		  }
-		  System.out.println();
-	  }
-  }
+	public static void main(String[] args) {
+		int isHungry = 0;
+		String food = "egg";
+		System.out.println("こんにちは");
+		if (isHungry == 5) {
+			System.out.println("おなかがいっぱいです");
+		} else {
+			System.out.println("腹ペコです");
+			System.out.println(food + "をいただきます");
+			System.out.println("ごちそうさまでした");
+		}
+	}
 }
