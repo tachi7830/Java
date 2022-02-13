8b1466e7f4f58636090173e1c6460008f6cb88a3
tachi7830 <xwsmilexw@gmail.com>
2022-02-13 21:34:58 +0900

    java book lesson4 finish

--- Java/src/Main4.java
+++ Java/src/Main4.java
@@ -1,8 +1,12 @@
 public class Main4 {
 	public static void main(String[] args) {
-		int[] scores = {20,30,40,50,60};
-		for (int val : scores) {
-			System.out.println(val);
+		int[] numbers = {3,4,9};
+		System.out.println("1桁の数字を入力してください");
+		int input = new java.util.Scanner(System.in).nextInt();
+		for (int i : numbers) {
+			if (i == input) {
+				System.out.println("アタリ");
+			}
 		}
 	}
 }
