09699c9f7b146c3f270a8d77f1099350e164fd5c
tachi7830 <xwsmilexw@gmail.com>
2022-02-15 00:16:54 +0900

    java book practice

--- Java/src/Main5.java
+++ Java/src/Main5.java
@@ -1,12 +1,9 @@
 public class Main5 {
     public static void main(String[] args) {    	
-    	int x = 3;
-    	int y = 4;
-    	int t;
-    	
-    	t = x;
-    	x = y;
-    	y = t;
-    	System.out.println(x + "," + y);
+    	int x = new java.util.Scanner(System.in).nextInt();
+    	if ( x % 2 == 0) {
+    		System.out.println("偶数です");
+    	} else {}
+    	    System.out.println("奇数です");
     }
 }
