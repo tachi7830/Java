181c10f1fc4b7d9d3074c2bbfabe255f1472fc51
tachi7830 <xwsmilexw@gmail.com>
2022-02-14 23:20:01 +0900

    java book practice

---
+++ Java/src/Main5.java
@@ -0,0 +1,12 @@
+public class Main5 {
+    public static void main(String[] args) {    	
+    	int x = 3;
+    	int y = 4;
+    	int t;
+    	
+    	t = x;
+    	x = y;
+    	y = t;
+    	System.out.println(x + "," + y);
+    }
+}
