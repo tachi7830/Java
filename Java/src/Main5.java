8f960c62c568ef9061f734d89cefde0330c45116
tachi7830 <xwsmilexw@gmail.com>
2022-02-15 01:39:45 +0900

    java book lesson5

--- Java/src/Main5.java
+++ Java/src/Main5.java
@@ -1,8 +1,18 @@
 public class Main5 {
 	public static void main(String[] args) {
 		int[] num = {1,2,3,4,5,6,7,8,9,10};
+		System.out.print("偶数");
 		for (int i = 0; i < 10; i++) {
-			System.out.println(num[i] * 2);
+			
+			if(i % 2 == 0) {
+				System.out.print(num[i]);
+			} 
 		}
-	}
+		System.out.print("奇数");
+		for (int i = 0; i < 10; i++) {
+			if(i % 2 != 0) {
+				System.out.print(num[i]);	
+			} 
+		}
+ 	}
 }
