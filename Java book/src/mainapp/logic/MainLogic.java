0ce5d47ce1ccc79e7ad28a117431527979e8fd7d
tachi7830 <xwsmilexw@gmail.com>
2022-02-14 15:48:59 +0900

    java book lesson6

---
+++ Java book/src/mainapp/logic/MainLogic.java
@@ -0,0 +1,11 @@
+package mainapp.logic;
+
+public class MainLogic {
+	public static int tasu(int a, int b) {
+		return (a + b);
+	}
+
+	public static int hiku(int a, int b) {
+		return (a - b);
+	}
+}
